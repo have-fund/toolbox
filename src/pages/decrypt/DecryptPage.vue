6be333cf63860107e49869e357856c9a7dddd292
a.tutunnik <a.tutunnik@corp.mail.ru>
2026-02-26 17:00:43 +0300

    subj

---
+++ src/pages/decrypt/DecryptPage.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import { useDecryptPage } from './hook'
+
+const { address, privateKey, encryptedText } = useDecryptPage()
+</script>
+
+<template>
+  <textarea v-model="encryptedText" name="encrypted-text" />
+  <div>{{ address }}</div>
+
+  <label>
+    <span>Private key</span>
+
+    <input
+      v-model="privateKey"
+      type="password"
+      name="private-key"
+      :disabled="!address"
+    />
+  </label>
+</template>
